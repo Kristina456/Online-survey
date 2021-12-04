b3b71bd77a688f1076e1cd60b26d2d124aa1b7cb
Kristina Peršić <72509184+Kristina456@users.noreply.github.com>
2021-12-04 11:44:39 +0100

    add SurvayPage structure

--- src/pages/SurvayPage.jsx
+++ src/pages/SurvayPage.jsx
@@ -1,5 +1,36 @@
 import React from "react";
 
 export default function SurvayPage() {
-	return <div>SurvayPage</div>;
+	return (
+		<div>
+			<div>Title</div>
+			<div>Description</div>
+			<form>
+				<div>
+					<label htmlFor="question">Some Question</label>
+					<input type="text" name="question" required />
+				</div>
+				<div>
+					<input type="radio" name="number-radio-input" value="1" />
+					<label>1</label>
+				</div>
+				<div>
+					<input type="radio" name="number-radio-input" value="2" />
+					<label>2</label>
+				</div>
+				<div>
+					<input type="radio" name="number-radio-input" value="3" />
+					<label>3</label>
+				</div>
+				<div>
+					<input type="radio" name="number-radio-input" value="4" />
+					<label>4</label>
+				</div>
+				<div>
+					<input type="radio" name="number-radio-input" value="5" />
+					<label>5</label>
+				</div>
+			</form>
+		</div>
+	);
 }
