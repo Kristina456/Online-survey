241c6f2f12e8885ba20744ca08b36c66aa5c15a4
Kristina Peršić <72509184+Kristina456@users.noreply.github.com>
2021-12-06 10:29:56 +0100

    add ErrorPage logic

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,10 +1,30 @@
 import React from "react";
+import { useNavigate } from "react-router";
+import { MockSurvay500 } from "../mocks/MockSurvay500";
 
 export default function ErrorPage() {
+	const navigate = useNavigate();
+
+	const error = MockSurvay500;
+	const showErrors = () => {
+		return error.errors.map((q) => {
+			return (
+				<div>
+					<div>{q.title}</div>
+					<div>{q.detail}</div>
+				</div>
+			);
+		});
+	};
+
+	const returnHome = () => {
+		navigate("/");
+	};
+
 	return (
 		<div>
-			<div>Title</div>
-			<div>Detail</div>
+			<div>{showErrors()}</div>
+			<button onClick={returnHome}>Return home</button>
 		</div>
 	);
 }
